71f9b36ae574e946dcbf231c7b0a00f7e155c38e
sophie <matthew.wetton@googlemail.com>
2018-04-06 19:12:15 +0100

    Update 01 - functions and loop

--- mwbuild.py
+++ mwbuild.py
@@ -20,15 +20,55 @@ def get_os_kuser_creds():
 
 # Collect credentials from OS Enviroment variables.
 kusercreds = get_os_kuser_creds()
-print "DEBUG : kusercreds : ",kusercreds
 
 # Keystone - Create authentication identity and session
 auth = identity.Password(**kusercreds)
 sess = session.Session(auth=auth)
 
+# Servers
+nova = noclient.Client(2, session=sess)
 # Networking
 neutron = neclient.Client(session=sess)
-nets = neutron.list_networks()
-print nets
+
+# Debug output
+#print "DEBUG : kusercreds : ",kusercreds
+#nets = neutron.list_networks()
+#print nets
+
+app_prefix="app001-"
+
+#router = neutron.show_router(router_id)
+
+# For as many instances as requested by the user, create networks and server.
+for x in range(0,3):
+  body_router = {'router':{'name': app_prefix + "router" + str(x), 'admin_state_up': True, 'external_gateway_info': {"network_id":"***************************************"}  }}
+  router = neutron.create_router(body_router)
+
+  # Create the network
+  body_network = {'network': {'name': "net" + str(x), 'admin_state_up': True}}
+  network = neutron.create_network(body_network)
+
+  # Retrive created network
+  created_network = neutron.list_networks(name="net" + str(x))
+  print created_network
+
+  # Retrive the network ID from the results
+  returned_network_id = created_network['networks'][0]['id']
+  print returned_network_id
+
+  # Create the subnet
+  body_subnet = {'subnets': [{'cidr': "192.168." + str(x) +".0/24", 'ip_version': 4, 'network_id': returned_network_id}]}
+  subnet = neutron.create_subnet(body_subnet)
+
+  # Create port
+#  body_port = {"port": {"admin_state_up": True, "device_id": server_id, "name": "port" + str(x), "network_id": returned_network_id }}
+#  port = neutron.create_port(body_port)
+
+  # Create server
+#  nova_client.servers.create(name="vm2", image=image, flavor=flavor, key_name="keypair-1", nics=nics)
+
+
+
+
 
 
